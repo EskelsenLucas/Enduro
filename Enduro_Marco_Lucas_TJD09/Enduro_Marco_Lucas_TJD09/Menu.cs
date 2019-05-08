d8aed735aa556c77c0f3a3de261f91bc07726e6d
EskelsenLucas <50421086+EskelsenLucas@users.noreply.github.com>
2019-05-08 16:22:24 -0300

    Setup Inicial

---
+++ Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enduro_Marco_Lucas_TJD09
+{
+
+    class Menu
+    {
+        public void ListaMenu()
+        {
+            Console.WriteLine("  Start \n" +
+                              " Options \n" +
+                              "  Exit");
+        }
+    }
+}
--- Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Program.cs
+++ Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Program.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace Enduro_Marco_Lucas_TJD09
 {
     class Program
     {
+        private const int largura = 75, altura = 30;
+        private const int MF_BYCOMMAND = 0x00000000;
+        //public const int SC_CLOSE = 0xF060;
+        public const int SC_MINIMIZE = 0xF020;
+        public const int SC_MAXIMIZE = 0xF030;
+        public const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            IntPtr handle = GetConsoleWindow();
+            IntPtr sysMenu = GetSystemMenu(handle, false);
+
+            if (handle != IntPtr.Zero)
+            {
+                //prevents user from closing application
+                //DeleteMenu(sysMenu, SC_CLOSE, MF_BYCOMMAND;
+                //prevents user from minimizing application
+                DeleteMenu(sysMenu, SC_MINIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+                DeleteMenu(sysMenu, SC_SIZE, MF_BYCOMMAND);
+            }
+
+            Console.BufferWidth = Console.WindowWidth = largura;
         }
     }
 }
