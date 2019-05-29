d86f1162105f54ec1d8a0d3207f40ace169aaee5
marconi9331 <marconi.youkai@gmail.com>
2019-05-29 09:08:32 -0300

    frames do carro arrumado

--- Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Carro.cs
+++ Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Carro.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Enduro_Marco_Lucas_TJD09
 {
@@ -10,15 +7,30 @@ namespace Enduro_Marco_Lucas_TJD09
 
     class Carro
     {
-        public List<char[,]> carro = new List<char[,]>()//Frame 1
-        { 
-            {'▄','╦','▄'},
-            {'▄','╬','▄'}
-        };
-        char[,] carro2 = new char[,] //Frame 2
+
+        int frameAtual;
+        int carX, carY;
+        ConsoleColor carColor;
+
+        public Carro(ConsoleColor Color = ConsoleColor.White, int posX = 50, int posY = 25)
         {
-            {'▀','╦','▀'},
-            {'▀','╬','▀'}
+            carX = posX;
+            carY = posY;
+            carColor = Color;
+        }
+
+        public List<char[,]> carroFrames = new List<char[,]>//Frame 1
+        {
+            new char[,]
+            {
+                {'▄','╦','▄'},
+                {'▄','╬','▄'}
+            },
+            new char[,] //Frame 2
+            {
+                {'▀','╦','▀'},
+                {'▀','╬','▀'}
+            },
         };
         int posXCarro;
 
@@ -32,9 +44,21 @@ namespace Enduro_Marco_Lucas_TJD09
                     posXCarro -= 1;
                     break;
             }
-            Console.SetCursorPosition((Program.Largura / 2 )+ 1 + posXCarro, 25);
-            Console.Write();
-            
+
+            frameAtual = ++frameAtual % carroFrames.Count;
+            var frame = carroFrames[frameAtual];
+
+
+            for (int y = 0; y < carroFrames[frameAtual].GetLength(1); y++)
+            {
+                for (int x = 0; x < carroFrames[frameAtual].GetLength(0); x++)
+                {
+                    Console.SetCursorPosition(carX + posXCarro + y, carY + x);
+                    Console.ForegroundColor = carColor;
+                    Console.Write(frame[x, y]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
