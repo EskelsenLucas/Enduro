74c9e20afd5ee41aa3bce1c006adb1a212f3be97
Marco Filipe <marconi.youkai@gmail.com>
2019-06-05 16:56:39 -0300

    arrumado o input repetindo

--- Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Player.cs
+++ Enduro_Marco_Lucas_TJD09/Enduro_Marco_Lucas_TJD09/Player.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Enduro_Marco_Lucas_TJD09
 {
@@ -10,68 +7,71 @@ namespace Enduro_Marco_Lucas_TJD09
     {
         Carro carroDoPlayer;
         ConsoleColor corDoCarro;
+        Thread inputReader;
+
 
         public Player(ConsoleColor corDoCarro)
         {
             this.corDoCarro = corDoCarro;
             carroDoPlayer = new Carro(corDoCarro);
+            inputReader = new Thread(new ThreadStart(inputtroll));
+            inputReader.Start();
+        }
+              
+
+        void inputtroll()
+        {
+            while (true)
+            {
+                LeInputDoJogador();
+            }
         }
 
-        public int MoveCarro()
+        public void LeInputDoJogador()
         {
-            return LeInputDoJogador().Item1;
-        }
+          
 
-
-        public int AceleraCarro()
-        {
-            return LeInputDoJogador().Item2;
-        }
-
-        public Tuple<int,int> LeInputDoJogador()
-        {
-            int x = 0, y = 0;
+            
             ConsoleKeyInfo teclaApertada = Console.ReadKey();
 
             switch (teclaApertada.Key)
             {
                 case ConsoleKey.LeftArrow:
-                    x = -1;
+                    carroDoPlayer.MoveCarro(-1, 0);
                     break;
                 case ConsoleKey.A:
-                    x = -1;
+                    carroDoPlayer.MoveCarro(-1, 0);
                     break;
                 case ConsoleKey.RightArrow:
-                    x = 1;
+                    carroDoPlayer.MoveCarro(1, 0);
                     break;
                 case ConsoleKey.D:
-                    x = 1;
+                    carroDoPlayer.MoveCarro(1, 0);
                     break;
-                case ConsoleKey.UpArrow:
-                    y = +6;
-                    break;
-                case ConsoleKey.W:
-                    y = +6;
-                    break;
-                case ConsoleKey.DownArrow:
-                    y = -6;
-                    break;
-                case ConsoleKey.S:
-                    y = -6;
-                    break;
-                default:
-                    x = 0;
-                    y = 0;
+                //case ConsoleKey.UpArrow:
+                //    y = -6;
+                //    break;
+                //case ConsoleKey.W:
+                //    y = -6;
+                //    break;
+                //case ConsoleKey.DownArrow:
+                //    y = 6;
+                //    break;
+                //case ConsoleKey.S:
+                //    y = 6;
+                //    break;
+                default:                   
                     break;
             }
-
-            return Tuple.Create(x,y);
+            while (Console.KeyAvailable)
+                Console.ReadKey(true);
         }
 
         public void DesenhaJogador()
         {
-            carroDoPlayer.MoveCarro(MoveCarro());
             carroDoPlayer.DesenhaCarro();
         }
+
+
     }
 }
